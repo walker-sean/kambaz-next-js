0f9df938807646faa6bf6a6d9735631fee855dfe
Sean Walker <walker.se@northeastern.edu>
2026-04-22 20:41:51 -0400

    sort by available date

--- app/(kambaz)/courses/[cid]/quizzes/page.tsx
+++ app/(kambaz)/courses/[cid]/quizzes/page.tsx
@@ -44,7 +44,7 @@ export default function QuizList() {
   const fetchQuizzes = useCallback(async () => {
     if (!cid || Array.isArray(cid)) return;
     findQuizzesForCourse(cid).then((quizzes) => {
-      setQuizzes(quizzes);
+      setQuizzes(quizzes.sort((a: any, b: any) => new Date(b.availableDate).getTime() - new Date(a.availableDate).getTime()));
     });
   }, [cid]);
 
